86d4edc37f630eb3dc75c1f104b1b90badd823d5
Habibaferdausi <ritu.pramanik1@gmail.com>
2023-05-15 18:05:38 +0600

    pagination added

--- index.js
+++ index.js
@@ -30,7 +30,15 @@ async function run() {
     const productCollection = client.db("emaJhonDB").collection("products");
 
     app.get("/products", async (req, res) => {
-      const result = await productCollection.find().toArray();
+      const page = parseInt(req.query.page) || 0;
+      const limit = parseInt(req.query.limit) || 10;
+      const skip = page * limit;
+
+      const result = await productCollection
+        .find()
+        .skip(skip)
+        .limit(limit)
+        .toArray();
       res.send(result);
     });
 
